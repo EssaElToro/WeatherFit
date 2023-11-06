88e2f0808d488e2ad38ad5fd70bf338531b313bb
Igor Niewiadomski <igor.niewiadomsky@gmail.com>
2023-11-06 12:14:03 +0100

    add expo app

---
+++ app/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Sraka.txt</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "whitesmoke",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
