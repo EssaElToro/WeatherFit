3a06ff227b1e4c689ae150b0315aa2104c3d7c71
Igor Niewiadomski <igor.niewiadomsky@gmail.com>
2023-11-27 10:31:19 +0100

    Update App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable, Button } from "react-native";
+import React from "react";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Sraka.txt</Text>
+      <View style={styles.bitmojiContainer}></View>
+      <View style={styles.buttonContainer}>
+        <Button title="Weather" onPress={() => {}} />
+        <Pressable style={{ flex: 1 }} onPress={() => {}}>
+          <Text>addada</Text>
+        </Pressable>
+        <Button title="Profile" onPress={() => {}} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "whitesmoke",
+    justifyContent: "flex-end",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  buttonContainer: {
+    flexGrow: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%",
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  bitmojiContainer: {
+    flexGrow: 100,
   },
 });
